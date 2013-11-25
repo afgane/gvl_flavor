685dc26b96217e8dda6b95710a5d685dd4aaac8d
Enis Afgan <afgane@gmail.com>
2013-11-25 14:28:23 -0500

    Add a script for installing Tool Shed tools via the API along with a sample file listing the tools to install

---
+++ install_tool_shed_tools.py
@@ -0,0 +1,56 @@
+"""
+A script to automate installation to tool repositories from a Galaxy Tool Shed
+into an instance of Galaxy.
+Installed tools need to be provided in YAML format in a file called ``tool_list.yaml``.
+See ``tool_shed_tool_list.yaml.sample`` for a sample of such a file.
+
+Usage:
+
+    python install_tool_shed_tools.py
+
+"""
+
+import yaml
+import datetime as dt
+from bioblend.galaxy import GalaxyInstance
+from bioblend.galaxy.toolshed import ToolShedClient
+from bioblend.toolshed import ToolShedInstance
+
+# Load tool list
+with open('tl.yaml', 'r') as f:
+    tl = yaml.load(f)
+
+gi = GalaxyInstance(tl['galaxy_instance'], tl['api_key'])
+r_info = tl['tools']
+
+responses = []
+
+for r in r_info:
+    if 'install_tool_dependencies' not in r:
+        r['install_tool_dependencies'] = True
+    if 'install_repository_dependencies' not in r:
+        r['install_repository_dependencies'] = True
+    if 'tool_shed_url' not in r:
+        r['tool_shed_url'] = 'http://toolshed.g2.bx.psu.edu'
+    ts = ToolShedInstance(url=r['tool_shed_url'])
+    if 'revision' not in r:
+        r['revision'] = ts.repositories.get_ordered_installable_revisions(
+            r['name'], r['owner'])[-1]
+
+    tsc = ToolShedClient(gi)
+    start = dt.datetime.now()
+    print 'Installing tool %s from %s' % (r['name'], r['owner'])
+    response = tsc.install_repository_revision(r['tool_shed_url'], r['name'],
+        r['owner'], r['revision'], r['install_tool_dependencies'],
+        r['install_repository_dependencies'], r['tool_panel_section_id'])
+        # new_tool_panel_section_label='API tests')
+    end = dt.datetime.now()
+    if 'error' in response:
+        print "Tool install error! Name: %s, owner: %s, error: %s" % (r['name'],
+            r['owner'], response['error'])
+    else:
+        print "Tool %s installed successfully (in %s)" % (r['name'], str(end - start))
+    outcome = {'tool': r, 'response': response, 'duration': str(end - start)}
+    responses.append(outcome)
+
+print responses
